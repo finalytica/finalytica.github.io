e58952f06f08da5f13e408cf18fcc6cb2e1995c1
EricPortela <ericq.portela@gmail.com>
2023-02-25 14:44:06 +0100

    Edited lottie file animation

--- scripts/lottie.js
+++ scripts/lottie.js
@@ -3,21 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
         player:'#mail-lottie',
         mode:"scroll",
         actions: [
+
+            {
+                visibility: [0.10, 0.20],
+                type:"seek",
+                frames: [0,10]
+            },
             {
                 visibility: [0.20, 1.0],
                 type: "loop",
-                frames: [0, 200]
-            }, 
-            {
-                visibility: [-0.10, 0.20],
-                type: "stop",
-                frames: [0]
+                frames: [10, 10]
             }
-
             // {
-            //     visibility: [0, 1.0],
-            //     type:"seek",
-            //     frames: [0,200]
+            //     visibility: [-0.10, 0.20],
+            //     type: "stop",
+            //     frames: [0]
             // }
         ]
     });
